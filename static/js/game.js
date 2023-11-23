9a18f14cdf8065cd903335a475e5a203c93fae30
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2023-11-23 01:19:58 -0300

    feat: Create a simple controller to player

--- static/js/game.js
+++ static/js/game.js
@@ -12,6 +12,15 @@ var game = {
     this.canvas.height =270;
     this.context = this.canvas.getContext("2d");
     this.interval = setInterval(updateCanvas, fpsToMiliseconds(45));
+    window.addEventListener("keydown", function(event) {
+      game.keys = (game.keys || []);
+      game.keys[event.keyCode] = true;
+      console.log(event.keyCode);
+    });
+    window.addEventListener("keyup", function(event) {
+      game.keys = (game.keys || []);
+      game.keys[event.keyCode] = false;
+    });
   },
 
   clear: function() {
@@ -28,15 +37,37 @@ function Player(width, height, color, x, y) {
   this.width = width;
   this.height = height;
   this.color = color;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.gravity = 0.05;
+  this.gravitySpeed = 0;
   this.x = x;
   this.y = y;
-  this.fall = function() {
-    if (this.y + this.height <= game.canvas.height) {
-      this.y += 5;
-    }
+  this.updatePosition = function() {
+    this.gravitySpeed += this.gravity;
+    this.x += this.speedX;
+    this.y += this.speedY + this.gravitySpeed ;
+    this.hitBottom();
   };
+
+  this.hitBottom = function() {
+    var floor = game.canvas.height - this.height;
+    if (this.y > floor) {
+      this.gravitySpeed = 0;
+      this.y = floor;
+    }
+  }
+
   this.update = function() {
-    this.fall();
+    MainPlayer.speedX = 0;
+    MainPlayer.speedY = 0;
+    MainPlayer.gravity = 0.1;
+    if (game.keys && game.keys[37]){ MainPlayer.speedX = -1; }
+    if (game.keys && game.keys[39]) {MainPlayer.speedX = 1; } 
+    if (game.keys && game.keys[38]) {MainPlayer.speedY = -1; MainPlayer.gravity = -0.2}
+    if (game.keys && game.keys[40]) {MainPlayer.speedY = 1; } 
+ 
+    this.updatePosition();
     ctx = game.context;
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
